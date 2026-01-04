2dc83572c9eb33e1561bacd2e162446024b68228
Ryder Schilling <ryderscott33@icloud.com>
2026-01-04 15:52:41 -0600

    Fix: Add missing CreateGoalButton import in GoalsList

--- components/goals/GoalsList.tsx
+++ components/goals/GoalsList.tsx
@@ -3,6 +3,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { formatCurrency, formatPercent, formatNumber, cn } from "@/lib/utils";
+import { CreateGoalButton } from "./CreateGoalButton";
 
 interface Goal {
   id: string;
